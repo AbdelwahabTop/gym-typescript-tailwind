3bb481edf722cf340fe1b786becf08bb6836b9cf
AbdelwahabTop <mramged936@gmail.com>
2023-03-11 21:03:48 +0200

    fonts and style configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
-  return <div className="app">app</div>;
+  return (
+    <div className="app bg-gray-20">
+      <navbar />
+    </div>
+  );
 }
 
 export default App;
