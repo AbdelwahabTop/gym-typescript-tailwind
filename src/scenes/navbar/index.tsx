a8e3dbd0bb9e554f1e22b3242eef1d0e57738917
AbdelwahabTop <mramged936@gmail.com>
2023-03-20 23:09:51 +0200

    start navbar component

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import { Bars3Icon, XMarkIcon } from "@heroicons/react";
+
+useState;
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
