950d0d179f581eca4af1500c7e320213e02ef549
AbdelwahabTop <mramged936@gmail.com>
2023-03-22 08:51:29 +0200

    Link

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Navbar from "@/scenes/navbar";
 function App() {
   return (
     <div className="app bg-gray-20">
-      <navbar />
-      <h1>El Gohary</h1>
+      <Navbar />
     </div>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
-
-import { Bars3Icon, XMarkIcon } from "@heroicons/react";
-
-useState;
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/logo.png";
+import Link from "./Link";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+  const flexBetween = "flex items-center justify-between";
+
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left side */}
+            <img alt="logo" src={Logo} />
+
+            {/* Right side */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link page="Home" />
+                <Link page="Benefits" />
+                <Link page="Our Classes" />
+                <Link page="Contact Us" />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
